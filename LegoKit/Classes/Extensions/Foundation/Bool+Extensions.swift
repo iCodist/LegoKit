fd4549fca55c825d967d6332806780fd521c6932
Unknown <forkon2008@gmail.com>
2018-09-03 09:54:27 +0800

    add Bool extensions

---
+++ LegoKit/Classes/Extensions/Foundation/Bool+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Bool+Extensions.swift
+//  LegoKit
+//
+//  Created by forkon on 2018/9/3.
+//
+
+import UIKit
+
+extension Bool {
+    
+    mutating func toggle() {
+        self = !self
+    }
+    
+}
