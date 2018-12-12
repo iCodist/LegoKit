8fc2178a994226398e85a079be6ac12392d1b93e
forkon <forkon2008@gmail.com>
2018-12-12 18:07:00 +0800

    add Tuple2Array and update String extensions

---
+++ LegoKit/Classes/Tuple2Array.swift
@@ -0,0 +1,20 @@
+//
+//  Tuple2Array.swift
+//  LegoKit
+//
+//  Created by forkon on 2018/12/12.
+//
+
+import UIKit
+
+class Tuple2Array: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
