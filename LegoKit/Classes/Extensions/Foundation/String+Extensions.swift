6ba9d7a2f1d7cd8bf0e7b8a38a074413fdaafd25
forkon <forkon2008@gmail.com>
2019-02-20 16:22:10 +0800

    update String extensions

--- LegoKit/Classes/Extensions/Foundation/String+Extensions.swift
+++ LegoKit/Classes/Extensions/Foundation/String+Extensions.swift
@@ -74,3 +74,15 @@ extension String {
     }
     
 }
+
+public extension String {
+    
+    func capitalizingFirstLetter() -> String {
+        return prefix(1).uppercased() + lowercased().dropFirst()
+    }
+    
+    mutating func capitalizeFirstLetter() {
+        self = capitalizingFirstLetter()
+    }
+    
+}
