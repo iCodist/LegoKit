6198381beb2f0fae7f8c3ea0c4c8de11944cc13f
forkon <forkon2008@gmail.com>
2019-10-12 15:49:52 +0800

    add UIImage extensions

---
+++ LegoKit/Classes/Extensions/UIKit/UIImage+Extensions.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Extensions.swift
+//  LegoKit
+//
+//  Created by forkon on 2019/10/12.
+//
+
+import UIKit
+
+public extension UIImage {
+
+    func applying(tintColor color: UIColor) -> UIImage{
+        UIGraphicsBeginImageContextWithOptions(self.size, false, self.scale)
+        let context: CGContext = UIGraphicsGetCurrentContext()!
+        context.translateBy(x: 0, y: self.size.height)
+        context.scaleBy(x: 1.0, y: -1.0)
+        context.setBlendMode(CGBlendMode.normal)
+        let rect: CGRect = CGRect(x: 0, y: 0, width: self.size.width, height: self.size.height)
+        context.clip(to: rect, mask: self.cgImage!)
+        color.setFill()
+        context.fill(rect)
+        let newImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()!;
+        UIGraphicsEndImageContext()
+        return newImage
+    }
+
+}
